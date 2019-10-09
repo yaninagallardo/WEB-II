61c542ec708856036705977b044ce17411c41689
Yani <yanina.gallardo30@gmail.com>
2019-10-09 16:58:17 -0300

    CAMBIO BD
    adapte la bd a lo que pide el tpe. para que nos sea mas facil trabajar

--- DDBB/wikiserie.sql
+++ DDBB/wikiserie.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-09-2019 a las 23:33:33
+-- Tiempo de generación: 09-10-2019 a las 21:57:09
 -- Versión del servidor: 10.1.38-MariaDB
 -- Versión de PHP: 7.3.3
 
@@ -25,15 +25,14 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `comentario`
+-- Estructura de tabla para la tabla `capitulos`
 --
 
-CREATE TABLE `comentario` (
-  `id_comentario` int(11) NOT NULL,
-  `nombre` varchar(45) NOT NULL,
+CREATE TABLE `capitulos` (
+  `id_capitulo` int(11) NOT NULL,
+  `titulo` varchar(100) NOT NULL,
   `temporada` int(11) NOT NULL,
   `capitulo` int(11) NOT NULL,
-  `comentario` varchar(140) NOT NULL,
   `id_serie` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -45,13 +44,21 @@ CREATE TABLE `comentario` (
 
 CREATE TABLE `serie` (
   `id_serie` int(11) NOT NULL,
-  `nombre` varchar(100) NOT NULL,
-  `sinopsis` varchar(140) NOT NULL,
-  `url_img` varchar(200) NOT NULL,
-  `arctor1` varchar(100) NOT NULL,
-  `actor2` varchar(100) NOT NULL,
-  `actor3` varchar(100) NOT NULL
+  `nombre` varchar(45) NOT NULL,
+  `sinopsis` varchar(400) NOT NULL,
+  `actor_principal` varchar(200) NOT NULL,
+  `tipo` varchar(40) NOT NULL,
+  `img` varchar(200) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `serie`
+--
+
+INSERT INTO `serie` (`id_serie`, `nombre`, `sinopsis`, `actor_principal`, `tipo`, `img`) VALUES
+(1, 'Arrow', '', '', 'Acción', 'public/images/arrow-portada-horizontal.png'),
+(2, 'Stranger Things', '', '', 'Terror', 'public/images/strager-portada-horizontal.png'),
+(3, 'Chicago PD', '', '', 'Acción', 'public/images/chicago-portada-horizontal.png');
 
 -- --------------------------------------------------------
 
@@ -62,20 +69,27 @@ CREATE TABLE `serie` (
 CREATE TABLE `usuario` (
   `id_usuario` int(11) NOT NULL,
   `nombre` varchar(45) NOT NULL,
-  `apellido` varchar(45) NOT NULL,
-  `user` varchar(200) NOT NULL,
-  `clave` varchar(20) NOT NULL
+  `user` varchar(60) NOT NULL,
+  `pass` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`id_usuario`, `nombre`, `user`, `pass`) VALUES
+(1, 'Yanina', 'yanina', '$2y$10$oZXXj4cRzqTSpP1gDKOUFeOWC/ZUISP.hIDxS.cNCe8RVtiUEbTYm');
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `comentario`
+-- Indices de la tabla `capitulos`
 --
-ALTER TABLE `comentario`
-  ADD PRIMARY KEY (`id_comentario`);
+ALTER TABLE `capitulos`
+  ADD PRIMARY KEY (`id_capitulo`),
+  ADD KEY `id_serie` (`id_serie`);
 
 --
 -- Indices de la tabla `serie`
@@ -94,22 +108,32 @@ ALTER TABLE `usuario`
 --
 
 --
--- AUTO_INCREMENT de la tabla `comentario`
+-- AUTO_INCREMENT de la tabla `capitulos`
 --
-ALTER TABLE `comentario`
-  MODIFY `id_comentario` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `capitulos`
+  MODIFY `id_capitulo` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `serie`
 --
 ALTER TABLE `serie`
-  MODIFY `id_serie` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_serie` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `capitulos`
+--
+ALTER TABLE `capitulos`
+  ADD CONSTRAINT `capitulos_ibfk_1` FOREIGN KEY (`id_serie`) REFERENCES `serie` (`id_serie`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
